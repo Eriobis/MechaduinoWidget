8b029dda651d03e8158183dce8a5dec5b8701124
S Benoit <spiky1987@hotmail.com>
2016-10-23 16:02:50 -0400

    Added class in the main window from the Terminal example

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,79 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_spinBox_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+
+}
+
+void MainWindow::transaction()
+{
+    setControlsEnabled(false);
+    statusLabel->setText(tr("Status: Running, connected to port %1.")
+                         .arg(serialPortComboBox->currentText()));
+    thread.transaction(serialPortComboBox->currentText(),
+                       waitResponseSpinBox->value(),
+                       requestLineEdit->text());
+}
+
+void MainWindow::showResponse(const QString &s)
+{
+    setControlsEnabled(true);
+    trafficLabel->setText(tr("Traffic, transaction #%1:"
+                             "\n\r-request: %2"
+                             "\n\r-response: %3")
+                          .arg(++transactionCount).arg(requestLineEdit->text()).arg(s));
+}
+
+void MainWindow::processError(const QString &s)
+{
+    setControlsEnabled(true);
+    statusLabel->setText(tr("Status: Not running, %1.").arg(s));
+    trafficLabel->setText(tr("No traffic."));
+}
+
+void MainWindow::processTimeout(const QString &s)
+{
+    setControlsEnabled(true);
+    statusLabel->setText(tr("Status: Running, %1.").arg(s));
+    trafficLabel->setText(tr("No traffic."));
+}
+
+void MainWindow::setControlsEnabled(bool enable)
+{
+    runButton->setEnabled(enable);
+    serialPortComboBox->setEnabled(enable);
+    waitResponseSpinBox->setEnabled(enable);
+    requestLineEdit->setEnabled(enable);
+}
